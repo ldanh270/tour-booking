f1ee5799f2aaaa909053eb5f9774906ae7701ce6
ldanh270 <ducanhle.dn@gmail.com>
2025-09-06 16:34:30 +0700

    style(blogs): change magin Read More btns

--- src/layout/home/blogs.tsx
+++ src/layout/home/blogs.tsx
@@ -86,7 +86,7 @@ export default function Blogs() {
                             <h3 className="text-foreground m-2 line-clamp-2 text-lg font-medium">
                                 {art.title}
                             </h3>
-                            <button className="border-primary-foreground text-foreground hover:bg-primary-foreground m-2 inline-flex w-fit cursor-pointer items-center gap-2 rounded-full border px-4 py-2 text-sm font-medium transition hover:text-white">
+                            <button className="border-primary-foreground text-foreground hover:bg-primary-foreground mx-2 my-4 inline-flex w-fit cursor-pointer items-center gap-2 rounded-full border px-4 py-2 text-sm font-medium transition hover:text-white">
                                 Read More →
                             </button>
                         </div>
