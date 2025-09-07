a4f6b9eb09252f8dcf9a9a6b6e8ccdeb9a362d77
ldanh270 <ducanhle.dn@gmail.com>
2025-09-07 15:04:53 +0700

    feat(footer): add layout footer

--- src/layout/core/footer.tsx
+++ src/layout/core/footer.tsx
@@ -1,3 +1,13 @@
+import Copywrite from '@/components/footer/copywrite'
+import Info from '@/components/footer/info'
+import Subscribe from '@/components/footer/subscribe'
+
 export default function Footer() {
-    return <div></div>
+    return (
+        <div>
+            <Subscribe />
+            <Info />
+            <Copywrite />
+        </div>
+    )
 }
